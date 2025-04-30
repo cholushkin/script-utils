4d33632622efad42d4e3ccca155fe489a92f2fec
Max <cholushkin@gmail.com>
2025-04-30 23:43:10 +0300

    add: GitSubmoduleManager.py

--- UserScriptsExamples/MakePromptAnalyzeScript.py
+++ UserScriptsExamples/MakePromptAnalyzeScript.py
@@ -6,7 +6,7 @@ PromptContextCollector(
     directories=["ScriptUtils/Core/PromptContextCollector/**"],  # Wildcards and folders
     includes=["*.py"],
     ignores=["*/Tests/*", "*.meta", "__pycache__"],
-    template_path="ScriptUtils/UserScriptsExamples/MakePromptAnalyzeScript.txt",  # Relative to project root
+    template_path="ScriptUtils/UserScriptsExamples/MakePromptAnalyzeScriptTemplate.txt",  # Relative to project root
     template_vars={"additional_task_0": "Share ideas to expand the concept and introduce innovative or interesting new features."},
     output_path="ScriptUtils/UserScriptsExamples/MakePromptOutput/PromptContextCollectorScriptAnalysis.txt"
 ).run()
